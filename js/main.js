af5efaae7eb2a306152dfffb2300040e116c3b46
davidnowottnik <david.nowottnik@fh-duesseldorf.de>
2015-11-05 16:41:28 +0100

    Mehrere Sprites animieren und bewegen

--- js/main.js
+++ js/main.js
@@ -17,30 +17,43 @@ window.onload = function() {
 
     function preload() {
         game.load.spritesheet('walk', 'img/walk.png', 104, 150, 14);
+        game.load.spritesheet('explosion', 'img/explosion.png', 36, 37, 14);
+        game.load.image('hat', 'img/hat.gif');
     }
 
     function create() {
-        player = game.add.sprite(game.world.centerX - 52, game.world.centerY - 75, 'walk');
-        player.animations.add('idle', [13], 1, false);
-        player.animations.add('walk', [6,7,8,9,10,11], 6, true);
-        player.anchor.setTo(.5,.5);
+        player = game.add.group();
+
+        var guy = game.add.sprite(game.world.centerX - 52, game.world.centerY - 75, 'walk');
+        guy.animations.add('idle', [13], 1, false);
+        guy.animations.add('walk', [6,7,8,9,10,11], 6, true);
+        guy.anchor.setTo(.5,.5);
+        player.add(guy);
+
+        var explosion = game.add.sprite(game.world.centerX - 44, game.world.centerY - 72, 'explosion');
+        explosion.animations.add('explode', [0,1,2,3,4,5,6,7,8], 30, true);
+        explosion.animations.add('idle', [0], 1, false);
+        explosion.anchor.setTo(.8,.5);
+        player.add(explosion);
 
         player.move = function(movement) {
-            this.animations.play('walk');
+            this.children[0].animations.play('walk');
+            this.children[1].animations.play('explode');
             player.x += movement;
         };
         player.moveLeft = function() {
-            this.scale.x = 1;
+            this.children[0].scale.x = 1;
             this.move(-3);
         };
         player.moveRight = function() {
-            this.scale.x = -1;
+            this.children[0].scale.x = -1;
             this.move(3);
         };
         player.idle = function() {
-            this.animations.play('idle', true);
+            this.children[0].animations.play('idle', true);
+            this.children[1].animations.play('idle', true);
         };
-        
+
         cursors = game.input.keyboard.createCursorKeys();
     }
 
